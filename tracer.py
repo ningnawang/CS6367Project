f9a585115cac1f6c7af4e0c30cc6af5b419b6adf
Anindita <aninditaashrafi@gmail.com>
2020-04-26 17:09:52 -0500

    python script added for invariant detection

---
+++ tracer.py
@@ -0,0 +1,38 @@
+import os
+
+file = open('trace2020-04-26T06:48:17.464Z.dat','r')
+lines = file.readlines()
+#print(len(lines))
+
+tracker = -1
+range_start = -1
+vals = []
+
+for i in range(0,len(lines)-1):
+    if(i==tracker):
+        print(lines[i])
+        tracker = -1
+    if(i>=range_start and range_start!=-1):
+        if(lines[i].find(':')<0):
+            vals.append(int(lines[i]))
+        elif(lines[i].find(':')>0):
+            range_start = -1
+            vals.sort()
+            if(len(vals)>1):
+                #print(vals[0])
+                #print(" ")
+                #print(vals[-1])
+                print('ranges from {} to {}'.format(vals[0],vals[-1]))
+                vals = []
+            elif(len(vals)==1):
+                print(vals[0])
+                vals = []
+    str = lines[i].split(':')
+    if((lines[i].find(':'))>0):
+        print(lines[i])
+        if(len(str)==2 and str[0]=='var_type' and str[1]==' boolean\n'):
+                tracker = i+2
+        elif(len(str)==2 and str[0]=='var_type'): #if other var type
+            range_start = i+2
+            #print("other var type found")
+
